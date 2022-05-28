0d59183fad429e577758c053f5ec9c15fc7bfbbf
Stanislav Kachnov <rileydarkest@gmail.com>
2022-05-28 17:25:48 +0300

    add sidemenu to login/register page

--- phyto_backend/urls.py
+++ phyto_backend/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = "phyto_backend"
 urlpatterns = [
+    re_path("^login/?$", views.AuthenticateView.as_view(), name="login"),
+    re_path("^signup/?$", views.RegistrationView.as_view(), name="register"),
     path('', views.index, name="home"),
     path('', include("django.contrib.auth.urls")),
     re_path("^login/?$", views.AuthenticateView.as_view(), name="login"),
